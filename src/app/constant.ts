93bda0ebd9bb962a866401969ee28ad70d2b193d
DIEGO ALPIN <diego_alpin@live.com>
2023-05-30 09:06:50 +0800

    Update constant.ts
    abs url

--- src/app/constant.ts
+++ src/app/constant.ts
@@ -1,5 +1,5 @@
 export class AppConstant {
-    private static API_BASE_URL = "157.230.194.31";
+    private static API_BASE_URL = "http://157.230.194.31";
         
     public static LOGIN_API_URL = this.API_BASE_URL+"/api/login";
     public static CAR_API_URL = this.API_BASE_URL+"/api/car";
